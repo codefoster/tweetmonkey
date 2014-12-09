02203ad7a6252965ebfad65a97b75184aa050045
Jeremy Foster <jeremy.foster@live.com>
2014-12-09 07:21:40 -0800

    change default topic search

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ cylon.robot({
     connections: { edison: { adaptor: 'intel-iot' } },
     devices: { monkey: { driver: 'direct-pin', pin: 2 } },
     work: function (edison) {
-        twit.stream('filter', { track: '#cfmonkey' }, function (stream) {
+        twit.stream('filter', { track: '#tweetmonkey' }, function (stream) {
             stream.on('data', function (data) {
                 edison.monkey.digitalWrite(1);
                 setTimeout(function () { edison.monkey.digitalWrite(0); }, 5000);
