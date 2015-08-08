61ec06960d8ce190c27577e4cc87d72369d1cb27
Jeremy Foster <jeremy.foster@live.com>
2015-08-08 00:21:45 -0700

    add comments to main app.js file

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var board = new five.Board({
   io: new Edison()
 });
 
+//configure the Twitter module with our consumer and access keys
+//obtain these by going to apps.twitter.com
 var twit = new Twitter({
     consumer_key: config.twitter.consumerKey,
     consumer_secret: config.twitter.consumerSecret,
@@ -19,11 +21,16 @@ var ON_DURATION = 2000;
 board.on("ready", function() {
   var monkey = new five.Pin("GP44");
     
-    //monitor by topic
+    //monitor tweets by tracking a hash tag
     twit.stream('filter', { track: '#tweetmonkey' }, function (stream) {
         stream.on('data', function (data) {
+            //log the tweet to the screen so we can see it if we have a console
             console.log('Tweet received from ' + data.user.screen_name + ': "' + data.text + '"');
+            
+            //take the pin high to animate the monkey
             monkey.high();
+            
+            //after some time, turn the monkey back off again
             setTimeout(function () { monkey.low(); }, ON_DURATION);
         });
     });
