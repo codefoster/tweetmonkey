2454f447d30bab304cb8f3be8e4a64f728292eb0
Jeremy Foster <jeremy.foster@live.com>
2017-02-20 14:38:32 -0800

    upgrade index.js to ES6

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-var five = require("johnny-five");
-var Pi = require("raspi-io");
-var Twitter = require('twitter');
+let five = require("johnny-five");
+let Pi = require("raspi-io");
+let Twitter = require('twitter');
 
-var board = new five.Board({
+let board = new five.Board({
   io: new Pi()
 });
 
-//configure the Twitter module with our consumer and access keys
-//obtain these by going to apps.twitter.com
-var twit = new Twitter({
+// configure the Twitter module with our consumer and access keys
+// obtain these by going to apps.twitter.com
+let client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var ON_DURATION = 2000;
+let ON_DURATION = 2000;
+let SEARCH_TERM = "#tweetmonkey";
 
-board.on("ready", function() {
-    var monkey = new five.Pin("GPIO23");
-    var light = new five.Led("GPIO24");
-    
+board.on("ready", () => {
+    let monkey = new five.Pin("GPIO23");
+    let light = new five.Led("GPIO24");
+
     //monitor tweets by tracking a hash tag
-    twit.stream('statuses/filter', { track: '#tweetmonkey' }, function (stream) {
+    client.stream('statuses/filter', { track: SEARCH_TERM }, stream => {
         //turn on an LED to indicate that we're waiting for a tweet
         light.on();
 
-        stream.on('data', function (tweet) {
+        stream.on('data', tweet => {
             //log the tweet to the screen so we can see it if we have a console
             console.log('Tweet received from ' + tweet.user.screen_name + ': "' + tweet.text + '"');
             
@@ -34,17 +35,17 @@ board.on("ready", function() {
             monkey.high();
             
             //after some time, turn the monkey back off again
-            setTimeout(function () { monkey.low(); }, ON_DURATION);
+            setTimeout(() => monkey.low(), ON_DURATION);
         });
+
+        stream.on('error', err => console.log(err));
     });
 
-    board.on("warn", function(event) {
+    board.on("warn", event => {
         //when the process shuts down, turn off the light
         if(event.class === 'Board' || event.message === 'Closing.') {
             monkey.low();
             light.off();
         }
     });
-
 });
-
